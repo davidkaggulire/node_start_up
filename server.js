d1d1538b2caaada765237b6feb6499f558e5162d
davidkaggulire <dkaggulire@gmail.com>
2022-03-29 19:44:49 +0300

    add server.js --entry file

---
+++ server.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+const app = require('./app');
+// dotenv must be called before app
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(DB);
+  console.log('DB connection successful');
+}
+
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`App running on http://127.0.0.1:${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message)
+  console.log('UNHANDLED REJECTION, shutting down')
+  server.close(() => {
+    process.exit(1)
+  })
+})
