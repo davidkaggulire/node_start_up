a32ab3821f91533bcaa844af4e7e1b0bc136d44f
davidkaggulire <dkaggulire@gmail.com>
2022-03-29 20:34:38 +0300

    edited server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ async function main() {
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
-  console.log(`App running on http://127.0.0.1:${port}...`);
+  console.log(`App running on ${port}...`);
 });
 
 process.on('unhandledRejection', err => {
