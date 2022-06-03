dedaa6867bfe8e77455be94c40273942d1b06d8e
davidkaggulire <dkaggulire@gmail.com>
2022-06-03 11:21:06 +0300

    add name to output

--- controllers/authController.js
+++ controllers/authController.js
@@ -40,14 +40,14 @@ const createSendToken = (user, statusCode, res) => {
 };
 
 exports.signUp = catchAsync(async (req, res, next) => {
-  // const newUser = await User.create({
-  //   name: req.body.name,
-  //   email: req.body.email,
-  //   password: req.body.password,
-  //   passwordConfirm: req.body.passwordConfirm,
-  // });
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
 
-  const newUser = await User.create(req.body);
+  // const newUser = await User.create(req.body);
 
   const url = `${req.protocol}://${req.get('host')}/me`;
   // console.log(url);
